09646967ca65b9d97aec5229085de3d8f9fde0ea
sibinos <sibin.os.g@gmail.com>
2022-09-26 14:21:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express =require('express')
 const app= express(); 
-const cors=require('cors')
 app.use(express.json())
-app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,token");
+    next();
+});
 
 const signupRoutes=require('./Routes/auth')
 const loginRoutes=require('./Routes/auth')
